829b7408f39f903a7ef520de4658cb8f63e92e8b
SilavongSone <silavongsone@gmail.com>
2023-02-23 15:26:29 +0700

    lorem-blue2

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -3,6 +3,7 @@
     <div class="grid grid-cols-4">
       <div class="bg-blue-500 p-2">
         <section><div><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolor ab qui voluptatum nesciunt adipisci animi minus corrupti repellat quas. Esse dolore voluptate repellendus fuga nisi doloribus? Voluptate, et nulla.</h1></div></section>
+        <section><div><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolor ab qui voluptatum nesciunt adipisci animi minus corrupti repellat quas. Esse dolore voluptate repellendus fuga nisi doloribus? Voluptate, et nulla.</h1></div></section>
       </div>
 
       <div class="col-span-3 bg-red-500 p-2">2</div>
