daab0f2e3a8339ad7d9e5430d29a8380d0bc82c7
Wen KuKu <Anusoesidalavilay@gmail.com>
2023-02-23 15:18:52 +0700

    profil lolem3

--- pages/students/index.vue
+++ pages/students/index.vue
@@ -1,18 +1,7 @@
 <template>
   <div>
     <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius
-      voluptatibus, veritatis enim labore non dolorem quidem ut qui, dolorum
-      officiis illo eligendi repudiandae. Ut optio cupiditate exercitationem
-      possimus atque?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius
-      voluptatibus, veritatis enim labore non dolorem quidem ut qui, dolorum
-      officiis illo eligendi repudiandae. Ut optio cupiditate exercitationem
-      possimus atque?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius
-      voluptatibus, veritatis enim labore non dolorem quidem ut qui, dolorum
-      officiis illo eligendi repudiandae. Ut optio cupiditate exercitationem
-      possimus atque?
+    
     </h1>
   </div>
 </template>
