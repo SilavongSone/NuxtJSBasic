fce69be4b7884e547a0fb38489c3b8ba9efadc67
Wen KuKu <Anusoesidalavilay@gmail.com>
2023-02-23 16:00:28 +0700

    stdtebo3

--- pages/students/index.vue
+++ pages/students/index.vue
@@ -1,18 +1,14 @@
 <template>
-  <div>
-<<<<<<< HEAD
-    <h1>Hello Worl</h1>
-=======
-    <h1>
-      Students
-    </h1>
-    <h1>
-      Students
-    </h1>
->>>>>>> 9b9b60a478eba84e5cb673e1dbe11f657b82873a
-  </div>
-</template>
-
-<script setup></script>
-
-<style scoped></style>
+    <div>
+      <h1>
+        Students
+      </h1>
+      <h1>
+        Students
+      </h1>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style lang="scss" scoped></style>
