3bfbcd5ec64a9c1a1cdd6c835f198cf172fa47ff
SilavongSone <silavongsone@gmail.com>
2023-02-23 15:24:49 +0700

    lorem-blue

--- pages/students/index.vue
+++ pages/students/index.vue
@@ -1,19 +1,6 @@
 <template>
   <div>
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius
-      voluptatibus, veritatis enim labore non dolorem quidem ut qui, dolorum
-      officiis illo eligendi repudiandae. Ut optio cupiditate exercitationem
-      possimus atque?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius
-      voluptatibus, veritatis enim labore non dolorem quidem ut qui, dolorum
-      officiis illo eligendi repudiandae. Ut optio cupiditate exercitationem
-      possimus atque?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius
-      voluptatibus, veritatis enim labore non dolorem quidem ut qui, dolorum
-      officiis illo eligendi repudiandae. Ut optio cupiditate exercitationem
-      possimus atque?
-    </h1>
+    <h1>Students</h1>
   </div>
 </template>
 
