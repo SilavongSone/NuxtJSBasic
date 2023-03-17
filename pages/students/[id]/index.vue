5dd385df5d3d8d8ad40b7092a15ffdce5560941f
SilavongSone <silavongsone@gmail.com>
2023-03-17 09:50:22 +0700

    const_database&edit_student(3/17/2023)2

--- pages/students/[id]/index.vue
+++ pages/students/[id]/index.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="flex h-4/5">
-
-
-      <section class="h-full w-full overflow-auto">
-        <ul class="p-1 bg-red-500" v-for="item in students" :key="item.id">
-          <NuxtLink :to="`/students/${item.id}`"
-            ><li class="bg-pink-500 grid grid-flow-col">
-              <p>{{ item.std_fname }} {{ item.std_lname }}</p>
-              <p>{{ item.std_id }}</p>
-              <!-- classroom -->
-              <p>{{ "-" }}</p>
-              <p>{{ "-" }}</p>
-            </li></NuxtLink
-          >
-        </ul>
-      </section>
-
+    <section class="h-full w-full overflow-auto">
+      <ul class="p-1 bg-red-500" v-for="item in students" :key="item.id">
+        <NuxtLink :to="`/students/${item.id}`"
+          ><li class="bg-pink-500 grid grid-flow-col">
+            <p>{{ item.std_fname }} {{ item.std_lname }}</p>
+            <p>{{ item.std_id }}</p>
+            <!-- classroom -->
+            <p>{{ "-" }}</p>
+            <p>{{ "-" }}</p>
+          </li></NuxtLink
+        >
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -27,15 +24,10 @@ definePageMeta({
   middleware: "user-only",
 });
 
-
 const { client } = useApolloClient();
 
 const students = ref<Student[]>([]);
 const search = ref("");
 
-
-
-watchEffect(() => {
-  
-});
+watchEffect(() => {});
 </script>
