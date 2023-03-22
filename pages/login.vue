d8cd2cd69305626c566fe921f6d3e688dae16bed
Wen KuKu <Anusoesidalavilay@gmail.com>
2023-03-22 17:03:13 +0700

    tc(3.22.23)

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
-    <section
-      class="flex flex-col p-10 justify-center items-center gap-2 bg-white rounded-md"
-    >
-      <h1 class="text-white bg-black p-2">LOGIN</h1>
-      <input class="border" v-model="email" type="email" required />
-      <input class="border" v-model="password" type="password" required />
-      <button class="bg-gray-100 hover:bg-gray-300 rounded-sm p-1" @click.prevent="handleLogin">Submit</button>
-    </section>
+    <div class="h-screen">
+      <section
+        class="flex flex-col p-10 justify-center items-center gap-2 rounded-md h-full bg-violet-50"
+      >
+        <div class="bg-violet-500 shadow-sm h-40 p-2">
+          <h1 class="flex text-white p-2 justify-center text-xl">
+            LOGIN
+          </h1>
+          <div class="flex flex-col">
+            <p>Email</p>
+            <input class="border" v-model="email" type="email" required />  
+            <p>password</p>
+            <input class="border" v-model="password" type="password" required />
+          </div>
+          <div class="flex justify-between bg-violet-500 ">
+            <button
+              class="bg-gray-100 hover:bg-gray-300 rounded-sm p-1 mt-4"
+              @click.prevent="handleLogin"
+            >
+              Login
+            </button>
+            <button class="bg-gray-100 hover:bg-gray-300 rounded-sm p-1 mt-4">
+            Register
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
